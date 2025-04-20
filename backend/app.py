23cb09320ce73ce92a6c72ec98e94a67d24ba7c5
aycaatac <candan.atac@ug.bilkent.edu.tr>
2025-04-20 04:36:57 +0300

    fix: database is not being recreated every time now

--- backend/app.py
+++ backend/app.py
@@ -16,15 +16,18 @@ DB_PORT = os.getenv("DB_PORT")
 """
 To drop learn_hub_db database, connect to postgres database 
 because we can not drop a database that we currently use.
-""" 
+"""
+
+
 def connect_postgres_db():
     return psycopg2.connect(
         dbname="postgres",
         user=POSTGRES_USER,
         password=POSTGRES_PASSWORD,
         host=DB_HOST,
-        port=DB_PORT
+        port=DB_PORT,
     )
+
 
 # To connect learn_hub_db
 def connect_project_db():
@@ -33,8 +36,9 @@ def connect_project_db():
         user=POSTGRES_USER,
         password=POSTGRES_PASSWORD,
         host=DB_HOST,
-        port=DB_PORT
+        port=DB_PORT,
     )
+
 
 # Drop database to reset it
 def reset_database():
@@ -43,12 +47,14 @@ def reset_database():
     cursor = conn.cursor()
 
     # Force disconnect all users
-    cursor.execute(f"""
+    cursor.execute(
+        f"""
         SELECT pg_terminate_backend(pg_stat_activity.pid)
         FROM pg_stat_activity
         WHERE pg_stat_activity.datname = '{POSTGRES_DB}'
             AND pid <> pg_backend_pid();
-    """)
+    """
+    )
     print(f"Disconnected all users from {POSTGRES_DB}")
 
     cursor.execute(f"DROP DATABASE IF EXISTS {POSTGRES_DB};")
@@ -60,13 +66,14 @@ def reset_database():
     cursor.close()
     conn.close()
 
+
 # Initialize the database using the schema.sql
 def initialize_tables():
     conn = connect_project_db()
     cursor = conn.cursor()
 
-    schema_path = os.path.join(os.path.dirname(__file__), 'schema.sql')
-    with open(schema_path, 'r') as f:
+    schema_path = os.path.join(os.path.dirname(__file__), "schema.sql")
+    with open(schema_path, "r") as f:
         schema_sql = f.read()
         cursor.execute(schema_sql)
 
@@ -75,11 +82,16 @@ def initialize_tables():
     conn.close()
     print(f"Initialized tables from schema.sql")
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Backend is running!"
 
-if __name__ == '__main__':
-    reset_database()     # Clean full database first
-    initialize_tables()  # Create tables
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+RESET_DB = os.getenv("RESET_DB", "false").lower() == "true"
+
+if __name__ == "__main__":
+    if RESET_DB:
+        reset_database()
+        initialize_tables()
+    app.run(host="0.0.0.0", port=5000, debug=True)
